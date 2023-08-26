cff78bf81dfc2fb53559731012a8c3363cda24ce
Tyler <48775431+Thymester@users.noreply.github.com>
2023-08-26 13:00:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
-import PrivacyPolicy from './components/PrivacyPolicy';
 import Resume from './components/Resume'; // Import the Resume component
 import './App.css';
 
