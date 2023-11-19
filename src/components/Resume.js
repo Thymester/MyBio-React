b66f8297e2498e3a345bed0ebeac603a67d3da3d
Tyler <48775431+Thymester@users.noreply.github.com>
2023-11-18 19:58:29 -0500

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,44 +6,6 @@ function Resume() {
       <h2>Work Experience</h2>
       <div className="resume-container">
         <div className="resume-entry">
-          <h3>Correctional Officer - Michigan Department of Corrections</h3>
-          <p>Baraga, Michigan - 10/2023 - Present</p>
-          <p><i>Attending Academy 10/02/2023 - 11/29/2023</i></p>
-          <ul>
-            <li>Observes prisoners' or detainees' activities to detect unusual or prohibited behavior and/or potential
-                threats to the safety and security of the facility, prisoners, detainees, employees, or visitors.</li>
-            <li>Responds quickly to calls for assistance in other areas of the prison or facility as directed. Assists in
-                controlling disturbances and isolating instigators.</li>
-            <li>Observes and appropriately responds to critical incidents, such as prisoner or detainee disturbances,
-                assaults on employees, prisoners, or detainees, or other situations that threaten the security of the
-                facility. Appropriate response may include the use of firearms.</li>
-            <li>Prepares written records and reports related to the work.</li>
-            <li>Conducts routine inspections of the facility to search for contraband items, such as weapons or drugs.</li>
-            <li>Inspects locks, window bars, grills, doors, and gates for tampering.</li>
-            <li>Inspects mail for the presence of contraband.</li>
-            <li>Inspects prisoners' or detainees' quarters for cleanliness and contraband.</li>
-            <li>Searches prisoners, detainees, and their quarters for contraband, such as weapons or drugs.</li>
-          </ul>
-          <h3>Correctional Officer Academy</h3>
-          <p>Location: Sault Sainte Marie, Michigan</p>
-          <p>Duration: 8 weeks</p>
-          <br />
-          <p>Curriculum:</p>
-          <ul>
-            <li>Physical Fitness</li>
-            <li>Firearms training</li>
-            <li>Chemecal agents</li>
-            <li>Arrest and control techniques</li>
-            <li>Laws of arrest</li>
-            <li>Report writing</li>
-            <li>Inmates rights</li>
-            <li>Inmate transportation</li>
-            <li>Disciplinary process</li>
-            <li>Restraint devices</li>
-            <li>The use of force</li>
-          </ul>
-        </div>
-        <div className="resume-entry">
           <h3>Mixer Truck Driver - Baraga County Concrete</h3>
           <p>Baraga, Michigan - 06/2023 - 10/2023</p>
           <ul>
@@ -56,7 +18,7 @@ function Resume() {
             <p>Marinette, Wisconsin - 04/2023 - 06/2023</p>
             <ul>
                 <li>Assure truck is up to DOT standards via a Pre-Trip inspection</li>
-                <li>Manage my clocks and trip plan to assure a on-time and safe delivery</li>
+                <li>Manage my clocks and trip plan to ensure an on-time and safe delivery</li>
                 <li>Maintain a professional appearance and approach at shippers and receivers</li>
             </ul>
         </div>
@@ -65,7 +27,7 @@ function Resume() {
             <p>Omaga, Nebraska - 02/2023 - 04/2023</p>
             <ul>
             <li>Assure truck is up to DOT standards via a Pre-Trip inspection</li>
-                <li>Manage my clocks and trip plan to assure a on-time and safe delivery</li>
+                <li>Manage my clocks and trip plan to ensure an on-time and safe delivery</li>
                 <li>Maintain a professional appearance and approach at shippers and receivers</li>
             </ul>
         </div>
@@ -74,7 +36,7 @@ function Resume() {
             <p>Spring Green, Wisconsin - 06/2021 - 02/2023</p>
             <ul>
                 <li>Lead a crew of 5-6 guys on a Furnace and help them develop the required skills for the job</li>
-                <li>Manage all the scheduling and assure everything is produced and shipped on time</li>
+                <li>Manage all the scheduling and ensure everything is produced and shipped on time</li>
                 <li>Asses situations with the lines and prioritize certain lines depending on external factors</li>
             </ul>
             </div>
@@ -83,8 +45,8 @@ function Resume() {
             <p>Remote in Madison, Wisconsin - 04/2020 - 12/2022</p>
             <ul>
                 <li>Test and assure the development team that the bug fixes and new implementations were appropriately fixed and accounted for</li>
-                <li>Write detailed write ups of my findings in a technical and concise way</li>
-                <li>Interact with customers and assure changes and implementations are working accordingly</li>
+                <li>Write detailed write-ups of my findings technically and concisely</li>
+                <li>Interact with customers and ensure changes and implementations are working accordingly</li>
             </ul>
                 </div>
             </div>
